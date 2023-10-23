65d7b8e64d6a348db6892ba128876fadb5d33b32
amisoni70 <amisoni7014@gmail.com>
2023-10-23 09:51:10 -0700

    Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that returns the to-do list progress for
+a given employees's ID
+"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    username = user.get("username")
+
+    with open("{}.csv".format(sys.argv[1]), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [sys.argv[1], username, t.get("completed"), t.get("title")]
+         ) for t in todos]
