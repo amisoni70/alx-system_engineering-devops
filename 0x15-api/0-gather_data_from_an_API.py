d42b21d058952354e4b41a4f3846f6836e040903
amisoni70 <amisoni7014@gmail.com>
2023-10-23 07:41:20 -0700

    added semicolon

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     tasks_done = [t for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{})".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(tasks_done), len(todos)))
     [print("\t {}".format(c.get("title"))) for c in tasks_done]
